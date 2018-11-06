87de4968a559a78db339afd40752f92320d3399f
wesbubeck <wesb@pendo.io>
2018-11-05 19:26:26 -0500

    setting up element components

---
+++ car-pool/src/components/customerInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="custInput">
+        <el-input placeholder="Name" v-model="name"></el-input>
+        <el-input placeholder="Address" v-model="address"></el-input>
+        <el-button type="primary" icon="el-icon-search">Search</el-button>
+    </div>
+</template>
+
+<script>
+
+import { Button } from 'element-ui';
+import { Input } from 'element-ui';
+
+export default {
+    name: 'CustomerInput',
+    data() {
+        return {
+        name: '',
+        address: ''
+        }
+    }
+}
+</script>
--- car-pool/src/views/Home.vue
+++ car-pool/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CustomerInput/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import CustomerInput from "@/components/customerInput.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+    name: "home",
+    components: {
+            CustomerInput,
+            HelloWorld
+    }
 };
 </script>
