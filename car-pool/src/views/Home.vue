9512390a18b8a622fabe7106dfcf68c41432d6cd
wesbubeck <wesb@pendo.io>
2018-11-19 22:33:32 -0500

    added veautify

--- car-pool/src/views/Home.vue
+++ car-pool/src/views/Home.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <CustomerInput/>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import CustomerInput from "@/components/customerInput.vue";
+  import HelloWorld from '../components/HelloWorld'
 
-export default {
-    name: "home",
+  export default {
     components: {
-            CustomerInput,
-            HelloWorld
+      HelloWorld
     }
-};
+  }
 </script>
